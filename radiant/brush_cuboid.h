9b2b166defb429b5e9efe1bbc1d4293242b95f90
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-02-12 13:07:25 -0600

    Update brush_cuboid.h
    Signed-off-by: *(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>

--- radiant/brush_cuboid.h
+++ radiant/brush_cuboid.h
@@ -1,1 +1,47 @@
+/*
+   Copyright (C) 1999-2007 id Software, Inc. and contributors.
+   For a list of contributors, see the accompanying CONTRIBUTORS file.
 
+   This file is part of GtkRadiant.
+
+   GtkRadiant is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GtkRadiant is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GtkRadiant; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _BRUSH_CUBOID_H_
+#define _BRUSH_CUBOID_H_
+
+#include "winding.h"
+
+class BrushCuboid {
+public:
+  BrushCuboid();
+  virtual ~BrushCuboid();
+
+  vec3_t cube_mins;
+  vec3_t cube_maxs;
+
+  virtual void CreateBrushCuboid(brush_t* p_brush, winding_t* p_winding, int p_facecount, const std::string& shader) = 0;
+  virtual void CreateBrushCuboid(const brush_t* b, const winding_t* w, const auto f_count, const std::string& shader) = 0;
+
+  BrushCuboid& getBrushCuboid(BrushCuboid& brush);
+  const BrushCuboid& getBrushCuboid(const BrushCuboid& brush);
+
+  auto& getBrushMins();
+  auto& getBrushMaxs();
+
+
+}
+
+#endif
