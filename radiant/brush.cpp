f57d394f6b6dbe3366a4779e5607beb262ae26d4
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-02-12 10:56:21 -0600

    Update brush.cpp
    Signed-off-by: *(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>

--- radiant/brush.cpp
+++ radiant/brush.cpp
@@ -37,7 +37,7 @@ auto g_nBrushId = 0;
 const std::string* Brush_Name( brush_t *b ){
 	brush_t* m_brush = nullptr;
     *m_brush = b;
-	static std::string* cBuff[1024];
+	static std::vector<std::string*> cBuff[1024];
 	m_brush->numberId = g_nBrushId++;
 	if ( g_qeglobals.m_bBrushPrimitMode != false ) {
 		sprintf( cBuff, "Brush %i", m_brush->numberId );
