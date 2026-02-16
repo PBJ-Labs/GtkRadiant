8f0826c7d7ce970532eb7df63c920c15e615cdcb
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-02-16 09:05:49 -0800

    Update layer.h
    Signed-off-by: *(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>

--- radiant/layer/layer.h
+++ radiant/layer/layer.h
@@ -9,8 +9,34 @@ public:
   Layer();
   virtual ~Layer() = default;
 
+  // get layer
+  const Layer& getLayer() const {
+    return *this;
+  }
+
+  // get layer name
+  const std::string& getLayerName() const {
+    return layerName;
+  }
+
+  int getLayerCount() const {
+    return layersInMap;
+  }
+
+  virtual void incrementLayers() {
+    layersInMap++;
+  }
+
+  virtual void decrementLayers() {
+    layersInMap--;
+  }
+
+  // create layer
+  virtual void createMapLayer(const Layer& layer, const std::string& name) = 0;
+
 protected:
-std::string layerName;
+std::string layerName = "LAYER-NAME";
+static int layersInMap;
 };
 
 #endif
