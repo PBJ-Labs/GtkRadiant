e098d2f3a2534aa6c1f4e60dc9038380db5781f7
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-02-16 12:50:21 -0600

    Update panel_component.h
    Signed-off-by: *(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>

--- radiant/sender/panel_component.h
+++ radiant/sender/panel_component.h
@@ -1,5 +1,72 @@
+/*
+   Copyright (C) 1999-2007 id Software, Inc. and contributors.
+   For a list of contributors, see the accompanying CONTRIBUTORS file.
+
+   This file is part of GtkRadiant.
+
+   GtkRadiant is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GtkRadiant is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GtkRadiant; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  
+*/
 
 #ifndef PANEL_COMPONENT_H
 #define PANEL_COMPONENT_H
+
+namespace sender {
+
+ class PanelComponent 
+ {
+  public:
+    PanelComponent();
+    virtual ~PanelComponent() = default;
+
+    const PanelComponent& getPanelComponent() const {
+      return *this;
+    }
+
+    const std::string& getPanelName() const {
+      return panelName;
+    }
+
+    virtual void panelComponentWidget(void* panel) = 0;
+
+    const std::string& getPanelBuffer() const {
+      return panelBuf;
+    }
+
+    const PanelComponent& releasePanel() const {
+      delete *[]this;
+    }
+
+    virtual void decrementCounter() {
+      componentCounter--;
+    }
+
+    virtual void incrementCounter() {
+      componentCounter++;
+    }
+
+    const PanelComponent& allocatePanel() const {
+      *this = new PanelComponent;
+    }
+
+ protected:
+ std::string panelName = "PANEL";
+ static int componentCounter;
+ static std::string panelBuf[2048];
+
+ };
+
+}
 
 #endif
